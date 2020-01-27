aef37fc8af24631b2d4ad81f15d572171199c930
erinhancock794 <erinhancock794@gmail.com>
2020-01-26 19:53:22 -0700

    moving all projects to one directory

---
+++ Unit1/js/main.js
@@ -0,0 +1,8 @@
+document.querySelector('#company').innerText = "Joe's Bed & Breakfast"
+
+document.querySelector('#slogan').innerText = "Coziest B&B in Utah!"
+
+let userName = prompt("What is your name?")
+let message = `Hello ${userName}, welcome to the most comfortable place to stay in Utah!`
+
+document.querySelector('#greeting').innerText = message

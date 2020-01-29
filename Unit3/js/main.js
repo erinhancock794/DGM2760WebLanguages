354641bba097dc1a7f5de673e1ce79191e0ba671
erinhancock794 <erinhancock794@gmail.com>
2020-01-28 17:00:47 -0700

    started JS for unit3

---
+++ Unit3/js/main.js
@@ -0,0 +1,22 @@
+
+// picking a random number between 0 and 21
+
+const correctNumber = Math.floor(Math.random() * (21 - 1) + 1)
+console.log(`The correct number is ${correctNumber}`)
+
+let guessed = false
+let totalGuesses = 0
+let gamerGuess = 0
+
+
+
+function evalGuess() {
+totalGuesses += 1 //totalGuesses = totalGuesses +1
+gamerGuess = document.querySelector('#guess').value
+
+const feedback = document.querySelector('#feedback')
+if (gamerGuess == correctNumber) {
+    console.log('gamerGuess is equal to correctNumber')
+}
+//console.log(totalGuesses, gamerGuess)
+}

5466474350643f32bc7e6a3ee34338c164788c1d
erinhancock794 <erinhancock794@gmail.com>
2020-02-04 15:54:03 -0700

    created Unit5

---
+++ Unit5/js/main.js
@@ -0,0 +1,14 @@
+//creating an array of trees
+const trees = ['pine', 'apple', 'oak', 'Douglas Fur']
+const errorElement = document.querySelector('#error')
+const displayResults = document.querySelector('#displayResults')
+displayResults.textContent = 'Trees?'
+errorElement.textContent = 'Huge error, bro.'
+
+
+//display list of trees inside displayResults div
+const listTrees = () => {
+    let treeList = ''
+    
+
+}

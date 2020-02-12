b8c5a011c511149af4e76eb23c1d578dc72ce3f6
erinhancock794 <erinhancock794@gmail.com>
2020-02-11 18:15:15 -0700

    writing nonsense story

--- Unit6/js/main.js
+++ Unit6/js/main.js
@@ -3,18 +3,21 @@ let resultStory = document.querySelector('#story')
 
 function tellStory(){
 
-let nounArray = document.querySelector('#noun').value.toLowerCase().split((/\s*[,\n." "]+\s*/))
+let nounArray = document.querySelector('#noun').value.toLowerCase().split(/\s*[,\n." "]+\s*/)
 console.log(nounArray)
-let adjArray = document.querySelector('#adjective').textContent
+let adjArray = document.querySelector('#adjective').value.toLowerCase().split(/\s*[,\n." "]+\s*/)
+console.log(adjArray)
+let verbArray = document.querySelector('#verb').value.toLowerCase().split(/\s*[,\n." "]+\s*/)
 
-let verbArray = document.querySelector('#verb').textContent
 
+const myStory = `Once upon a time there were some <span>${nounArray[0]}s </span> named Looloo, Smash, and Peter. Peter has a 
+ <span>${adjArray[0]}</span> <span>${verbArray[0]}</span>, and sometimes he would sneak around the <span>${nounArray[5]}</span> and creep behind Looloo, raise his <span>${nounArray[4]}</span>, and scare her. 
+ She'd get so scared that she'd jump on her <span> ${adjArray[1]}</span> <span> ${nounArray[1]} and <span>${verbArray[1]}</span> around the halls.`
 
-const myStory = `Once upon a time there were some <span>${nounArray[0]}s </span> named Looloo, Smash, and Peter. ${adjArray[0]} ${verbArray[0]}`
-console.log(myStory)
 //get a reference to the onclick id
-const postStory = document.querySelector('#story')
-postStory.outputHTML = myStory
+resultStory.innerHTML = myStory
+console.log(myStory)
+
 
 }
 document.querySelector('#story').onclick = tellStory

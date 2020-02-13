2d9c404c7a1ae95769bdf7d90d6f47549eb12c54
erinhancock794 <erinhancock794@gmail.com>
2020-02-13 11:27:44 -0700

    added nonsense story

--- Unit6/js/main.js
+++ Unit6/js/main.js
@@ -8,11 +8,28 @@ console.log(nounArray)
 let adjArray = document.querySelector('#adjective').value.toLowerCase().split(/\s*[,\n." "]+\s*/)
 console.log(adjArray)
 let verbArray = document.querySelector('#verb').value.toLowerCase().split(/\s*[,\n." "]+\s*/)
+/* Wizard x
+Broom x
+Elf x
+Cauldron x
+Wand x
+Castle x
+Spell */
 
+/* Scary x
+Sparkly x
+Small x */
 
-const myStory = `Once upon a time there were some <span>${nounArray[0]}s </span> named Looloo, Smash, and Peter. Peter has a 
- <span>${adjArray[0]}</span> <span>${verbArray[0]}</span>, and sometimes he would sneak around the <span>${nounArray[5]}</span> and creep behind Looloo, raise his <span>${nounArray[4]}</span>, and scare her. 
- She'd get so scared that she'd jump on her <span> ${adjArray[1]}</span> <span> ${nounArray[1]} and <span>${verbArray[1]}</span> around the halls.`
+/* Laugh x
+Fly  x*/
+
+const myStory = `Once upon a time there were some <span>${nounArray[0]}s </span> named Looloo, Smash, and Peter. Peter had a 
+ <span>${adjArray[0]}</span> <span>${verbArray[0]}</span>, and sometimes he would sneak around the <span>${nounArray[5]}</span> and creep behind Looloo, 
+ raise his <span>${nounArray[4]}</span>, and scare her. 
+ She'd get so scared that she'd jump on her <span> ${adjArray[1]}</span> <span> ${nounArray[1]} and <span>${verbArray[1]}</span> around the halls. 
+ She usually ended up at the gamekeeper's hut on the grounds. The gamekeeper was a <span> ${adjArray[2]} <span>${nounArray[2]}</span>. He would help her with her homework 
+ because her <span> ${nounArray[3]}s </span> kept exploding in class. She'd shine it until it was <span> ${adjArray[1]}</span> but then it would explode
+ the next potions class.`
 
 //get a reference to the onclick id
 resultStory.innerHTML = myStory

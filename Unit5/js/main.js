85795a889ebf04ff010673eed0e669aa8de95cd6
erinhancock794 <erinhancock794@gmail.com>
2020-03-12 14:50:40 -0600

    fixed lowercase sorting

--- Unit5/js/main.js
+++ Unit5/js/main.js
@@ -31,29 +31,29 @@ document.querySelector('#add_apple').onclick = () => {
 }
 //remove first tree in list
 document.querySelector('#remove_tree1').onclick = () => {
-    if(trees.length > 0){
-    trees.shift()
-    listTrees()
+    if (trees.length > 0) {
+        trees.shift()
+        listTrees()
     } else {
         errorElement.textContent = "Can't remove a tree because there aren't any listed."
     }
 }
 //remove second tree in list
 document.querySelector('#remove_tree2').onclick = () => {
-    if(trees.length > 1){
-    trees.splice(1,1)
-    listTrees()
-    errorElement.textContent = ''
+    if (trees.length > 1) {
+        trees.splice(1, 1)
+        listTrees()
+        errorElement.textContent = ''
     } else {
         errorElement.textContent = "There aren't enough trees to remove the second one."
     }
 }
 //remove last tree in list
 document.querySelector('#remove_treeLast').onclick = () => {
-    if(trees.length >0){
-    trees.pop()
-    listTrees()
-    errorElement.textContent = ''
+    if (trees.length > 0) {
+        trees.pop()
+        listTrees()
+        errorElement.textContent = ''
     } else {
         errorElement.textContent = "Can't remove a tree because there aren't any listed."
     }
@@ -62,27 +62,28 @@ document.querySelector('#remove_treeLast').onclick = () => {
 
 //sort trees A-Z
 document.querySelector('#sortTrees').onclick = () => {
-    if(trees.length >0) {
-        trees.sort((a,b) => {
-         return a.toLowerCase().localeCompare(b.toLowerCase())})
-    listTrees() 
-   
-   errorElement.textContent = ''}
-  else {
+    if (trees.length > 0) {
+        trees.sort((a, b) => {
+            return a.toLowerCase().localeCompare(b.toLowerCase())
+        })
+        listTrees()
 
-     errorElement.textContent = "Can't sort without any trees in the list."}
- } 
+        errorElement.textContent = ''
+    }
+    else {
+
+        errorElement.textContent = "Can't sort without any trees in the list."
+    }
+}
 //make all trees lowercase
 
 document.querySelector('#lowercaseTrees').onclick = () => {
-    if(trees.length > 0){
-  let nameList = ''
-  errorElement.textContent = ''
-  trees.forEach(tree => {
-      nameList += `${tree.toLowerCase()} <br>`
-  })
-   displayResults.innerHTML = nameList
-} else {
+    if (trees.length > 0) {
+        let nameList = []
+        nameList = trees.map(trees => trees.toLowerCase())
+        trees.splice(0, nameList.length, ...nameList)
+        listTrees() 
+    } else {
     errorElement.textContent = "Can't sort without any trees in the list."
 }
 }
@@ -95,16 +96,18 @@ document.querySelector('#lowercaseTrees').onclick = () => {
 
 //name of the third tree
 document.querySelector('#name_tree3').onclick = () => {
-    if(trees.length > 2) {
-      error.textContent = trees[2]
-} else {
-    error.textContent = 'There are not enough trees to list the third tree.'
-}}
+    if (trees.length > 2) {
+        error.textContent = trees[2]
+    } else {
+        error.textContent = 'There are not enough trees to list the third tree.'
+    }
+}
 
 //name of the 4th tree and error message
 document.querySelector('#name_tree4').onclick = () => {
-    if(trees.length > 3) {
-      error.textContent = trees[3]
-} else {
-    error.textContent = 'There are not enough trees to list the fourth tree.'
-}}
+    if (trees.length > 3) {
+        error.textContent = trees[3]
+    } else {
+        error.textContent = 'There are not enough trees to list the fourth tree.'
+    }
+}

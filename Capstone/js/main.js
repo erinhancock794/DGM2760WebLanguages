efc0360f99257e9204c4e3851143ae9e3eb49dba
erinhancock794 <erinhancock794@gmail.com>
2020-03-12 17:49:15 -0600

    started styling

--- Capstone/js/main.js
+++ Capstone/js/main.js
@@ -13,7 +13,7 @@ async function getYogaData(){
 }
 let yogaData = {}
 getYogaData().then(data => (yogaData = data))
-
+console.log(yogaData)
 
 
 document.querySelector("#big_toe_pose").addEventListener('click', poseInfo)
@@ -21,13 +21,12 @@ document.querySelector("#chair_pose").addEventListener('click', poseInfo)
 document.querySelector("#downward_facing_dog_pose").addEventListener('click', poseInfo)
 
 function poseInfo(event) {
-    let yogaChoice = yogaData.poses.find(pose => {
-        return event.target.id === pose.name.toLowerCase()
+    let yogaChoice = yogaData.poses.find(poses => {
+        return event.target.id === poses.name.toLowerCase()
     }) 
    
     console.log(yogaChoice)
 document.querySelector("#yoga_name").textContent = `${yogaData.name} Poseee`
 document.querySelector("#alternate").textContent = `${yogaData.alternate} `
 
-}
-
+} 

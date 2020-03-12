6c785c2a1060b7f41ed03a69116ab17f38e90a9b
erinhancock794 <erinhancock794@gmail.com>
2020-03-12 17:37:36 -0600

    adding JSON info to DOM

---
+++ Capstone/js/main.js
@@ -0,0 +1,33 @@
+
+document.querySelector("#websiteName").textContent = "The Yoga Studio"
+document.querySelector("#slogan").textContent = "Learn Yoga"
+
+async function getYogaData(){
+    try {
+        const response = await fetch('yoga.json')
+        return await response.json() //returns JSON object
+
+    } catch(error) {
+        console.error(error)
+    }
+}
+let yogaData = {}
+getYogaData().then(data => (yogaData = data))
+
+
+
+document.querySelector("#big_toe_pose").addEventListener('click', poseInfo)
+document.querySelector("#chair_pose").addEventListener('click', poseInfo)
+document.querySelector("#downward_facing_dog_pose").addEventListener('click', poseInfo)
+
+function poseInfo(event) {
+    let yogaChoice = yogaData.poses.find(pose => {
+        return event.target.id === pose.name.toLowerCase()
+    }) 
+   
+    console.log(yogaChoice)
+document.querySelector("#yoga_name").textContent = `${yogaData.name} Poseee`
+document.querySelector("#alternate").textContent = `${yogaData.alternate} `
+
+}
+
